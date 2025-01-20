14af957d760cacd1f7ca59b51f421f6d057329c3
DikuCoding <dikeshkhagi1@gmail.com>
2025-01-20 18:55:16 +0545

    add PortfolioSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/Navbar";
+import PortfolioSection from "@/components/PortfolioSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
+      <PortfolioSection/>
     </div>
   );
 }
